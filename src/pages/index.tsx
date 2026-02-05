17f014e9b4cdec3ed60d7fa33f757499b0fc8070
Stephen01623 <ralphjoedg@gmail.com>
2026-02-06 00:53:00 +0800

    fix all error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
+import { AboutSection } from "@/components/AboutSection"
 import { HeaderSection } from "@/components/HeaderSection"
+import { HeroSection } from "@/components/HeroSection"
 
 const index = () => {
   return (
     <div className="min-h-screen bg-background">
       <HeaderSection />
+      <HeroSection />
+      <AboutSection />
     </div>
    
   )
