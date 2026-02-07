235ad4dfd965b9a2baccad803a0bc6eb64585935
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-02-07 23:00:05 +0800

    setup: added Tournament model

---
+++ server/Models/Tournament.cs
@@ -0,0 +1,15 @@
+﻿namespace server.Models
+{
+    public class Tournament
+    {
+        public int? TournamentID { get; set; }
+
+        public List<Team>? ParticipatingTeams { get; set; }
+
+        public DateTime? DateCreated { get; set; }
+
+        public int? Duration { get; set; }
+
+        public string? Location { get; set; }
+    }
+}
