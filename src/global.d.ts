18610abfd4e4638aa57a62b02966b647a8235811
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-02-17 19:41:33 +0800

    docs: deleted files and moved to client folder

--- src/global.d.ts
+++
@@ -1,7 +0,0 @@
-// Tell TypeScript that importing CSS files is okay
-declare module "*.css";
-declare module "*.jpg";
-declare module "*.jpeg";
-declare module "*.png";
-declare module "*.svg";
-declare module "*.webp";
