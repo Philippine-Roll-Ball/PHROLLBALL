652c2af4aa207527da591d16bb56f7c12ce8adaa
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-02-17 22:41:05 +0800

    setup: added Match model

---
+++ server/Models/Match.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Models
+{
+    public class Match
+    {
+        [Required]
+        public int? MatchID { get; set; }
+
+        public List<Team>? Teams { get; set; }
+        [Required]
+        public string? Result { get; set; }
+        [Required]
+        public DateTime? MatchDate { get; set; }
+
+        [Required]
+        public int Duration { get; set; }
+    }
+}
