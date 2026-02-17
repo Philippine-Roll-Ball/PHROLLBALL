c1bc410aa3eebc33183d83b32c3339244506350f
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-02-17 22:40:40 +0800

    setup: added Coach.cs model

---
+++ server/Models/Coach.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Models
+{
+    public class Coach
+    {
+        [Required]
+        public int? CoachID { get; set; }
+
+        [Required]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? FullName { get; set; } = $"{FirstName} {MiddleName} {LastName} {Suffix}";
+
+        [Required]
+        public string? ContactNumber { get; set; }
+        [Required]
+        public static string? FirstName { get; set; }
+        [Required]
+        public static string? MiddleName { get; set; }
+        [Required]
+        public static string? LastName { get; set; }
+        [Required]
+        public static string? Suffix { get; set; }
+        [Required]
+        public DateTime BirthDate { get; set; }
+        [Required]
+        public string? EducationalAttainment { get; set; }
+
+        public string? ProfileImageUrl { get; set; }
+        [Required]
+        public string? Occupation { get; set; }
+      
+        public List<string?>? OtherSports { get; set; }
+        [Required]
+        public string? Role { get; set; }
+        [Required]
+        public DateTime DateRegistered { get; set; }
+        [Required]
+        public Team? TeamAssigned { get; set; }
+
+    }
+}
