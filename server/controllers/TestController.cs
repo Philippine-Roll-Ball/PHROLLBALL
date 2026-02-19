e10121924c7c0945db9dd260a7fb711cb4ea0414
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-02-19 12:20:26 +0800

    Added test controller

---
+++ server/controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetTestJson()
+        {
+            Console.WriteLine("Received GET request at /api/test");
+
+            return Ok(new { Message = "What the heck?!" });
+
+        }
+    }
+}
