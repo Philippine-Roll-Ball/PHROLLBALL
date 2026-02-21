adbf966343dc23a2284e80009de99c1295d333f8
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-02-21 20:00:00 +0800

    setup: added Admin.cs Model

---
+++ server/Models/Admin.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int AdminID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string? Username { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string? Password { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string? Email { get; set; }
+    }
+}
