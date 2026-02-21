cb4470e9a656c67e3a69201bba3ea41444c6289b
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-02-21 20:00:00 +0800

    refactor: Added Necessary Attributes for each Player.cs property

--- server/Models/Player.cs
+++ server/Models/Player.cs
@@ -1,43 +1,64 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace server.Models
 {
     public class Player
     {
-        [Required]
+        [Key]
         public int? PlayerID { get; set; }
 
         [Required]
+        [MaxLength(50)]
         public string? Email { get; set; }
 
         [Required]
-        public string? FullName { get; set; } = $"{FirstName} {MiddleName} {LastName} {Suffix}";
+        [MaxLength(11)]
+        public string? ContactNumber { get; set; }
 
         [Required]
-        public static string? FirstName { get; set; }
+        [MaxLength(50)]
+        public string? FirstName { get; set; }
 
-        public static string? MiddleName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string? MiddleName { get; set; }
 
-        public static string?  LastName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string?  LastName { get; set; }
 
-        public static string? Suffix { get; set; }
+        [MaxLength(5)]
+        public string? Suffix { get; set; }
+
+        [NotMapped]
+        public string? FullName => $"{FirstName} {MiddleName} {LastName} {Suffix}".Trim();
+
 
         public DateTime BirthDate { get; set; }
 
+
+
+        [MaxLength(30)]
         public string? EducationalAttainment { get; set; }
+
+        [MaxLength(255)]
 
         public string? ProfileImageUrl { get; set; }
 
+        [MaxLength(30)]
         public string? Occupation { get; set; }
 
         public List<string?>? OtherSports { get; set; }
 
+        [MaxLength(10)]
         public string? Role { get; set; }
 
         public DateTime DateRegistered { get; set; }
-        public int MyProperty { get; set; }
 
+        public int? TeamID { get; set; }
+
+        [ForeignKey(nameof(TeamID))]
         public Team? TeamAssigned { get; set; }
-
     }
 }
