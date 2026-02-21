dd4a88f83a536479618c6944ab392cc43d96f62d
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-02-21 20:00:00 +0800

    setup: created AppDbContext

--- server/Data/AppDbContext.cs
+++ server/Data/AppDbContext.cs
@@ -11,14 +11,25 @@ namespace server.Data
         {
         }
 
+        public DbSet<Admin> Admins { get; set; }
         public DbSet<Player> Players { get; set; }
         public DbSet<Match> Matches { get; set; }
-
         public DbSet<Coach> Coaches { get; set; }
-
         public DbSet<Team> Teams { get; set; }
-
         public DbSet<Tournament> Tournaments { get; set;  }
+        public DbSet<TournamentTeam> TournamentTeams { get; set; }
+
+        public DbSet<MatchTeam> MatchTeams { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Player>()
+                .HasOne(t => t.TeamAssigned)
+                .WithMany(p => p.Players)
+                .HasForeignKey(p => p.TeamAssigned.TeamID);
+
+
+        }
 
     }
 }
