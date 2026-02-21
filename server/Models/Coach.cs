c33649868b4a115ecf8fd6f1ab282900ce4e74b7
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-02-21 20:00:00 +0800

    refactor: Added Necessary Attributes for each property

--- server/Models/Coach.cs
+++ server/Models/Coach.cs
@@ -1,44 +1,63 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace server.Models
 {
     public class Coach
     {
-        [Required]
-        public int? CoachID { get; set; }
+        [Key]
+        public int CoachID { get; set; }
 
         [Required]
+        [MaxLength(50)]
         public string? Email { get; set; }
 
         [Required]
-        public string? FullName { get; set; } = $"{FirstName} {MiddleName} {LastName} {Suffix}";
+        [MaxLength(50)]
+        public string? Password { get; set; }
 
         [Required]
+        [MaxLength(11)]
         public string? ContactNumber { get; set; }
+
         [Required]
-        public static string? FirstName { get; set; }
-        [Required]
-        public static string? MiddleName { get; set; }
-        [Required]
-        public static string? LastName { get; set; }
-        [Required]
-        public static string? Suffix { get; set; }
-        [Required]
+        [MaxLength(50)]
+        public string? FirstName { get; set; }
+
+        [MaxLength(50)]
+        public string? MiddleName { get; set; }
+
+        [MaxLength(50)]
+        public string? LastName { get; set; }
+
+        [MaxLength(5)]
+        public string? Suffix { get; set; }
+
+        [NotMapped]
+        public string? FullName => $"{FirstName} {MiddleName} {LastName} {Suffix}".Trim();
+
         public DateTime BirthDate { get; set; }
-        [Required]
+
+        [MaxLength(30)]
         public string? EducationalAttainment { get; set; }
 
+        [MaxLength(255)]
         public string? ProfileImageUrl { get; set; }
-        [Required]
-        public string? Occupation { get; set; }
-      
-        public List<string?>? OtherSports { get; set; }
-        [Required]
-        public string? Role { get; set; }
-        [Required]
-        public DateTime DateRegistered { get; set; }
-        [Required]
-        public Team? TeamAssigned { get; set; }
 
+        [MaxLength(20)]
+        public string? Occupation { get; set; }
+
+        public List<string?>? OtherSports { get; set; }
+
+        [MaxLength(10)]
+        public string? Role { get; set; }
+
+        public DateTime DateRegistered { get; set; }
+
+        // FK to Team
+        public int? TeamAssigned { get; set; }
+
+        [ForeignKey(nameof(TeamAssigned))]
+        public Team? Team { get; set; }
     }
 }
