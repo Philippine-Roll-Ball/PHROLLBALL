1481ba01fb2d1339140584c906363b84b6a21713
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-02-21 20:00:00 +0800

    setup: Added TournamentTeam.cs model for junction Table

---
+++ server/Models/TournamentTeam.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace server.Models
+{
+    public class TournamentTeam
+    {
+
+        [Required]
+        public int TournamentID { get; set; }
+
+        [Required]
+        public int TeamID { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string? TeamStanding { get; set; }
+
+        [ForeignKey(nameof(TournamentID))]
+        public Tournament? Tournament { get; set; }
+
+        [ForeignKey(nameof(TeamID))]
+        public Team? Team { get; set; }
+
+    }
+}
