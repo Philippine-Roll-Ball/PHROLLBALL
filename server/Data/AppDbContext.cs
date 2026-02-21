5ae433b8460f01f59ca39b40e23e38351265b1ed
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-02-21 20:00:00 +0800

    Created DbSet for Coaches, Players, and Matches

---
+++ server/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using server.Models;
+
+
+
+namespace server.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Match> Matches { get; set; }
+
+        public DbSet<Coach> Coaches { get; set; }
+
+    }
+}
