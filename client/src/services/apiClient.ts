cf6ce0d45c26927f25d87e09d3f3f1c940d058c1
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-03-06 22:51:05 +0800

    refactor: added error handling statement for response status code 401

--- client/src/services/apiClient.ts
+++ client/src/services/apiClient.ts
@@ -14,6 +14,11 @@ export const apiClient = axios.create( {
 apiClient.interceptors.response.use(
     (response) => response,
     (error) => {
-        
+        if(error.response?.status === 401) {
+            console.error("Session Expired. Please Login Again.");
+            window.location.href = "/";
+        }
+        return Promise.reject(error);
     }
+    
 )
