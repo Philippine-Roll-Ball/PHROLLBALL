8100a3aa5eb71735028b052eb81dd33c07f70af2
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-03-06 22:49:02 +0800

    feat: added apiClient Interceptors

--- client/src/services/apiClient.ts
+++ client/src/services/apiClient.ts
@@ -1,3 +1,19 @@
 import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_URL;
+
+export const apiClient = axios.create( {
+    baseURL: API_BASE_URL,
+    withCredentials: true,
+    headers: {
+        "Content-type": "application/json",
+    },
+});
+
+
+apiClient.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        
+    }
+)
