9930da997a7282a97f7ce9c4156577731e7698e3
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-03-06 23:15:26 +0800

    backend-feat: added controller for authenticatino

---
+++ server/controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace server.controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            Console.WriteLine($"Connected Successfully {request.Email} {request.Password}");
+            return Ok(new { message = "Connected!" });
+        }
+    }
+}
