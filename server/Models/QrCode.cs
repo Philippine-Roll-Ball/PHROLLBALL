b14611b00c8329423dc40b182230cc1b22d06b78
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-03-21 06:34:54 +0800

    feat: added Model for QrCode

---
+++ server/Models/QrCode.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace server.Models
+{
+    public class QrCode
+    {
+        [Key]
+        [JsonPropertyName("securityToken")]
+        public string? SecurityToken { get; set; }
+
+        [JsonPropertyName("entityType")]
+        public string? EntityType { get; set; }
+        [JsonPropertyName("data")]
+        public string? Data { get; set; }
+        
+        [JsonPropertyName("createdByEmail")]
+        public string? CreatedByEmail { get; set; }
+    }
+}
