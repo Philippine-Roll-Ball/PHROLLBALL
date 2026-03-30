0fa066fa8c47aeab83f2575b917dacef7fae3b2e
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-03-30 19:04:11 +0800

    feat: created a User type to reduce redundancy

---
+++ client/src/types/user.ts
@@ -0,0 +1,23 @@
+
+
+export interface User {
+    firstName: string;
+    
+}
+
+// firstName: "",
+//     middleName: "",
+//     lastName: "",
+//     birthDate: "",
+//     age: "",
+//     nationality: "",
+//     sex: "",
+//     phone: "",
+//     region: "",
+//     province: "",
+//     city: "",
+//     barangay: "",
+//     role: "",
+//     email: "",
+//     password: "",
+//     confirmPassword: "",
