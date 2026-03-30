dd8a1eed77edbb7d138265386b3c49e665d3c6d1
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-03-30 23:40:20 +0800

    refactor: returned the Region related lists and handle changes

--- client/src/hook/useRegion.tsx
+++ client/src/hook/useRegion.tsx
@@ -2,9 +2,10 @@ import { useState, useEffect } from "react";
 import { regions, provinces, cities, barangays } from "select-philippines-address";
 import { Region, Province, Municipality, Barangay } from "@/types/user";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const useAddress = (setFormData: any) => {
-    const [regionList, setRegionList] = useState<Regionp[]>([]);
-    const [provinceList, setProvinceList] = useState<Provice[]>([]);
+    const [regionList, setRegionList] = useState<Region[]>([]);
+    const [provinceList, setProvinceList] = useState<Province[]>([]);
     const [cityList, setCityList] = useState<Municipality[]>([]);
     const [barangayList, setBarangayList] = useState<Barangay[]>([]);
     
@@ -14,6 +15,7 @@ export const useAddress = (setFormData: any) => {
 
     const handleRegionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         const code = e.target.value;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         setFormData((prev: any) => ({...prev, region: code, province: "", city: "", barangay: ""}));
         provinces(code).then((res) => setProvinceList(res));
         setCityList([]);
@@ -23,6 +25,7 @@ export const useAddress = (setFormData: any) => {
 
     const handleProvinceChange =(e: React.ChangeEvent<HTMLSelectElement>) => {
         const code = e.target.value;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         setFormData((prev: any) => ({...prev, province: code, city: "", barangay: ""}));
         cities(code).then((res) => setCityList(res));
         setBarangayList([]);
@@ -30,9 +33,15 @@ export const useAddress = (setFormData: any) => {
 
     const handleCityChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         const code = e.target.value;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         setFormData((prev: any) => ({...prev, city: code, barangay: ""}));
         barangays(code).then((res) => setBarangayList(res));
     };
+
+    return {
+        regionList, provinceList, cityList, barangayList,
+        handleRegionChange, handleProvinceChange, handleCityChange
+    }
     
 
 }
