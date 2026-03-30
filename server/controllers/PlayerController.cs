dee5d3131cc5e515ee34bacd2c2abcfb0137b426
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-03-30 23:10:20 +0800

    feat: added the asynchronous saving function for the registation

--- server/controllers/PlayerController.cs
+++ server/controllers/PlayerController.cs
@@ -20,7 +20,14 @@ public class PlayerController : ControllerBase {
     public async Task<IActionResult> Register([FromBody] Player request)
     {
 
-        var existing = await _playerContext.Players.FirstOrDefaultAsync(p => p.Email == request.Email )
+        var existing = await _playerContext.Players.FirstOrDefaultAsync(p => p.Email == request.Email &&
+            p.FirstName == request.FirstName && 
+            p.LastName == request.LastName);
+        if (existing != null) return BadRequest("Player Already Exists");
+
+        await _playerContext.Players.AddAsync(request);
+        await _playerContext.SaveChangesAsync();
+
 
         return Ok("Registration Successful!");
     }
