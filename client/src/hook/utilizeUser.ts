9a14a9355ce7861afe6cce55700b5f47e7cc8044
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-04-04 18:54:10 +0800

    refactor: changed filename into utilizeUser

---
+++ client/src/hook/utilizeUser.ts
@@ -0,0 +1,41 @@
+
+
+// sample hook structure I should follow 
+// import { useQuery } from '@tanstack/react-query';
+// import { getPlayerById } from '../services/playerService';
+
+// export const usePlayer = (id: string) => {
+//   return useQuery({
+//     queryKey: ['player', id],
+//     queryFn: () => getPlayerById(id), // Calling the service
+//   });
+// };
+
+ 
+import { registerPlayer } from "@/services/userService";
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { User } from "@/types/user";
+
+export const usePlayer = () => {
+    const queryClient = useQueryClient();
+
+    const registerQuery = useMutation({
+        mutationFn: (user: User ) => registerPlayer(user),
+
+        onSuccess: (data) =>  {
+            queryClient.invalidateQueries({ queryKey: ['players']});
+            console.log("player registered", data);
+        },
+        onError: (error) => {
+            console.error("Error registering player:", error);
+        }
+    });
+    return {
+        registerPlayerAsync: registerQuery.mutateAsync,
+        registerPlayer: registerQuery.mutate,
+        isRegistering: registerQuery.isPending,
+        error: registerQuery.error,
+        isSuccess: registerQuery.isSuccess,
+    }
+}
+
