c3882df87eb29588a009476158e7cd83ecf5321d
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-04-05 15:15:17 +0800

    feat: added AuthenticationService that will handlw JWT issuance

---
+++ server/Services/AuthenticationService.cs
@@ -0,0 +1,8 @@
+﻿namespace server.Services
+{
+    public class AuthenticationService
+    {
+        // This will serve as the authentication service for the application. It will issue JWT tokens upon successful login 
+        // and after registration.
+    }
+}
