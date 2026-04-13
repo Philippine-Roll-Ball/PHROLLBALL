d298252098238097dbef14f21c5b40852930a148
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-04-13 23:47:01 +0800

    temporary removed checks

--- server/Program.cs
+++ server/Program.cs
@@ -96,12 +96,9 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
-
-if(app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 
 app.UseHttpsRedirection();
