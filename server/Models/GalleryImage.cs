3b8046b8088ac0ea1a489ea9fac15642bfc86551
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-06-06 17:10:40 +0800

    feat: added GalleryImage Model for single files

---
+++ server/Models/GalleryImage.cs
@@ -0,0 +1,48 @@
+﻿using Google.Cloud.Firestore;
+
+namespace server.Models
+{
+    [FirestoreData]
+    public class GalleryImage
+    {
+
+        [FirestoreDocumentId]
+        public string Id { get; set; }
+
+        public IFormFile File { get; set; }
+
+        [FirestoreProperty("image_url")]
+        public string? ImageUrl { get; set; }
+
+        /**
+         * StoragePath
+         * Filename
+         * UploadedBy
+         * UploadDate
+         * Width
+         * Height
+         * Tags
+        **/
+        [FirestoreProperty("storage_path")]
+        public string? StoragePath { get; set; }
+
+        [FirestoreProperty("filename")]
+        public string? FileName { get; set; }
+
+        [FirestoreProperty("uploaded_by")]
+        public string? UploadedBy { get; set; }
+
+        [FirestoreProperty("upload_date")]
+        public DateTime? UploadDate { get; set; }
+
+        [FirestoreProperty("width")]
+        public int? Width { get; set; }
+        [FirestoreProperty("height")]
+        public int? Height { get; set; }
+
+        [FirestoreProperty("tags")]
+        public List<string>? Tags { get; set; } = new List<string>();
+
+
+    }
+}
