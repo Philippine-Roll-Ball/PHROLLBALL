57a5f418014c4969dbf176d1154be7aac8d173a3
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-06-06 17:17:40 +0800

    refactor: changed the double ticks into backtics

--- client/src/services/galleryService.ts
+++ client/src/services/galleryService.ts
@@ -1,6 +1,18 @@
 import { apiClient } from "./apiClient";
 
 export const getGalleryItems = async () => {
-    const response = await apiClient.get("api/gallery");
+    const response = await apiClient.get(`api/gallery`);
     return response.data;
-}
+};
+
+export const uploadGalleryImages = async (formData: FormData) => {
+    const response = await apiClient.post(`api/gallery/upload`, formData, {
+        headers: {
+            "Content-Type": "multipart-formdata",
+
+        },
+    });
+    return response.data;
+
+};
+
