12c9ff97eed1f876a6a608118f9b8950b0521f39
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-06-10 18:14:16 +0800

    feat: added fetching by id directly to database

--- server/Repositories/MemberRepository.cs
+++ server/Repositories/MemberRepository.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using server.Data;
+using server.Models;
 namespace server.Repositories
 {
     public class MemberRepository : IMemberRepository
     {
         private readonly AppDbContext _context;
-        public MemberRepository(AppDbContext context) => _context = context; 
+        public MemberRepository(AppDbContext context) => _context = context;
+        
+        public async Task<User?> GetByIdAsync(Guid id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
     }
 }
