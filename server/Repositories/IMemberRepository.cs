e6888e14aeb2d53bc7755f876fc9fc9266a85d62
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-06-10 18:03:31 +0800

    feat: added interface for member repository

---
+++ server/Repositories/IMemberRepository.cs
@@ -0,0 +1,16 @@
+﻿using server.Models;
+
+namespace server.Repositories
+{
+    public interface IMemberRepository
+    {
+        Task<User?> GetByIdAsync(Guid id);
+        Task<IEnumerable<User>> GetAllAsync();
+
+        Task AddAsync(User user);
+
+        Task UpdateAsync(User user);
+
+        Task DeleteAsync(Guid id);
+    }
+}
