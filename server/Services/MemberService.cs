414b30e7b92f04ff8648daf77a06ef0a531e8dbf
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-06-10 18:04:20 +0800

    feat: added Member service for separation of concerns

---
+++ server/Services/MemberService.cs
@@ -0,0 +1,44 @@
+﻿using server.DTOs;
+using server.Repositories;
+using server.Services.Interfaces;
+
+namespace server.Services
+{
+    public class MemberService : IMemberService
+    {
+        private readonly IMemberRepository _repo;
+
+        public MemberService(IMemberRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<UserDto?> GetUserAsync(Guid id)
+        {
+            var user = await _repo.GetByIdAsync(id);
+
+            if (user == null) return null;
+
+            return new UserDto
+            {
+                UserID = user.UserID,
+                Email = user.Email,
+                ContactNumber = user.ContactNumber,
+                FirstName = user.FirstName,
+                MiddleName = user.MiddleName,
+                LastName = user.LastName,
+                Suffix = user.Suffix,
+                BirthDate = user.BirthDate,
+                Address = user.Address,
+            };
+        }
+
+        
+
+        public async Task<UserDto?> CreateAsync(UserDto user)
+        {
+            // replace with actual implementation soon
+            return new UserDto();
+        }
+    }
+}
