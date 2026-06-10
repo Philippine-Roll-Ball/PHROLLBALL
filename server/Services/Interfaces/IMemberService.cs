709fae47a55ab115ea15b3f5d51d569abb69c393
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-06-10 18:04:42 +0800

    feat: added Interface for MemberService

---
+++ server/Services/Interfaces/IMemberService.cs
@@ -0,0 +1,16 @@
+﻿using server.DTOs;
+
+namespace server.Services.Interfaces
+{
+    public interface IMemberService
+    {
+        Task<UserDto?> GetUserAsync(Guid id);
+        Task<IEnumerable<UserDto>> GetAllAsync();
+
+        Task<UserDto?> CreateAsync(UserDto user);
+
+        Task<UserDto?> UpdateAsync(Guid id, UserDto user);
+
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
