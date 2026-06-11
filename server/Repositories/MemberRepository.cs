b9c97a35274dd40711d0c1181f5e3b471b4848d9
N0OBitaz <charlesbernard.balaguer.041504@gmail.com>
2026-06-11 21:20:35 +0800

    feat: added async function for creation of member

--- server/Repositories/MemberRepository.cs
+++ server/Repositories/MemberRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
 using server.Data;
 using server.Models;
 namespace server.Repositories
@@ -11,6 +12,18 @@ namespace server.Repositories
         public async Task<User?> GetByIdAsync(Guid id)
         {
             return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User?> AddAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
         }
     }
 }
